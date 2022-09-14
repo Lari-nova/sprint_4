8eb128a4e2f9e01dadde6c8a200bd6ef095260b8
Anastasia Larionova <larinova13@gmail.com>
2022-09-14 20:05:13 +0300

    Attempt number three

--- src/test/java/ru/yandex/praktikum/DropdownTest.java
+++ src/test/java/ru/yandex/praktikum/DropdownTest.java
@@ -17,18 +17,106 @@ public class DropdownTest extends BaseUITest {
     }
 
     @Test
-    public void checkActivity() {
+    public void checkActivityOne() {
         driver.get("https://qa-scooter.praktikum-services.ru/");
-
         mainPage.clickAndScroll();
 
-        for (int i = 0; i <= 7; i++) {
-            DropdownImportantQuestions dropdownQuestions = new DropdownImportantQuestions(
-                    driver, String.format("accordion__heading-%s", i),
-                    String.format("accordion__panel-%s", i)
-            );
-            dropdownQuestions.clickOnAccordionHeaderOne();
-            MatcherAssert.assertThat(dropdownQuestions.getAccordionText(), is(answers[i]));
-        }
+        DropdownImportantQuestions dropdownQuestions = new DropdownImportantQuestions(
+                driver, String.format("accordion__heading-%s", 0),
+                String.format("accordion__panel-%s", 0)
+        );
+        dropdownQuestions.clickOnAccordionHeaderOne();
+        MatcherAssert.assertThat(dropdownQuestions.getAccordionText(), is(answers[0]));
+    }
+
+    @Test
+    public void checkActivityTwo() {
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+        mainPage.clickAndScroll();
+
+        DropdownImportantQuestions dropdownQuestions = new DropdownImportantQuestions(
+                driver, String.format("accordion__heading-%s", 1),
+                String.format("accordion__panel-%s", 1)
+        );
+        dropdownQuestions.clickOnAccordionHeaderOne();
+        MatcherAssert.assertThat(dropdownQuestions.getAccordionText(), is(answers[1]));
+    }
+
+    @Test
+    public void checkActivityThree() {
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+        mainPage.clickAndScroll();
+
+        DropdownImportantQuestions dropdownQuestions = new DropdownImportantQuestions(
+                driver, String.format("accordion__heading-%s", 2),
+                String.format("accordion__panel-%s", 2)
+        );
+        dropdownQuestions.clickOnAccordionHeaderOne();
+        MatcherAssert.assertThat(dropdownQuestions.getAccordionText(), is(answers[2]));
+    }
+
+    @Test
+    public void checkActivityFour() {
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+        mainPage.clickAndScroll();
+
+        DropdownImportantQuestions dropdownQuestions = new DropdownImportantQuestions(
+                driver, String.format("accordion__heading-%s", 3),
+                String.format("accordion__panel-%s", 3)
+        );
+        dropdownQuestions.clickOnAccordionHeaderOne();
+        MatcherAssert.assertThat(dropdownQuestions.getAccordionText(), is(answers[3]));
+    }
+
+    @Test
+    public void checkActivityFive() {
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+        mainPage.clickAndScroll();
+
+        DropdownImportantQuestions dropdownQuestions = new DropdownImportantQuestions(
+                driver, String.format("accordion__heading-%s", 4),
+                String.format("accordion__panel-%s", 4)
+        );
+        dropdownQuestions.clickOnAccordionHeaderOne();
+        MatcherAssert.assertThat(dropdownQuestions.getAccordionText(), is(answers[4]));
+    }
+
+    @Test
+    public void checkActivitySix() {
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+        mainPage.clickAndScroll();
+
+        DropdownImportantQuestions dropdownQuestions = new DropdownImportantQuestions(
+                driver, String.format("accordion__heading-%s", 5),
+                String.format("accordion__panel-%s", 5)
+        );
+        dropdownQuestions.clickOnAccordionHeaderOne();
+        MatcherAssert.assertThat(dropdownQuestions.getAccordionText(), is(answers[5]));
+    }
+
+    @Test
+    public void checkActivitySeven() {
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+        mainPage.clickAndScroll();
+
+        DropdownImportantQuestions dropdownQuestions = new DropdownImportantQuestions(
+                driver, String.format("accordion__heading-%s", 6),
+                String.format("accordion__panel-%s", 6)
+        );
+        dropdownQuestions.clickOnAccordionHeaderOne();
+        MatcherAssert.assertThat(dropdownQuestions.getAccordionText(), is(answers[6]));
+    }
+
+    @Test
+    public void checkActivityEight() {
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+        mainPage.clickAndScroll();
+
+        DropdownImportantQuestions dropdownQuestions = new DropdownImportantQuestions(
+                driver, String.format("accordion__heading-%s", 7),
+                String.format("accordion__panel-%s", 7)
+        );
+        dropdownQuestions.clickOnAccordionHeaderOne();
+        MatcherAssert.assertThat(dropdownQuestions.getAccordionText(), is(answers[7]));
     }
 }
